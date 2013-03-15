f57e19900a53cce78660be63f981d12ebb0ece24
Ralph Schindler <ralph@ralphschindler.com>
2013-03-15 16:50:29 -0500

    Initial Authorization API

--- source/GitHubAPI/Authorization/Authorization.php
+++ source/GitHubAPI/Authorization/Authorization.php
@@ -6,5 +6,59 @@ use GitHubAPI\AbstractEntity;
 
 class Authorization extends AbstractEntity
 {
-    // @todo
+    protected $id;
+    protected $url;
+    protected $scopes;
+    protected $token;
+    protected $app;
+    protected $note;
+    protected $noteUrl;
+    protected $updatedAt;
+    protected $createdAt;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function getScopes()
+    {
+        return $this->scopes;
+    }
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    public function getNoteUrl()
+    {
+        return $this->noteUrl;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
 }
--- source/GitHubAPI/Authorization/AuthorizationAPI.php
+++ source/GitHubAPI/Authorization/AuthorizationAPI.php
@@ -6,5 +6,22 @@ use GitHubAPI\AbstractAPI;
 
 class AuthorizationAPI extends AbstractAPI
 {
-    // @todo
+    public function createAuthorization(array $parameters = array())
+    {
+        $validParameters = array(
+            'scopes' => null,
+            'note' => null,
+            'note_url' => null,
+            'client_id' => null,
+            'client_secret' => null,
+        );
+        $params = $this->processParameters($validParameters, $parameters);
+        $api = 'POST /authorizations';
+        $authData = $this->doAPIRequest($api, $params);
+        if ($authData === false) {
+            return false;
+        }
+        $auth = Authorization::createEntity($authData);
+        return $auth;
+    }
 }
